71a05e62e15dfb81342afe4bfcf8b7e5f8b688a7
Aorko Chakma <aorkochkmaa16@gmail.com>
2026-03-28 12:06:34 -0400

    Update index.php

--- crud-CI/index.php
+++ crud-CI/index.php
@@ -297,7 +297,7 @@ $total = count($users);
 
 <header>
   <span class="logo-badge">ARGOCD</span>
-  <h1>PHP CRUD Number 1722 Demo</h1>
+  <h1>PHP CRUD Number app/crud-CI    Demo</h1>
   <span>v1.0.0 · <?= date('Y-m-d') ?></span>
 </header>
 
